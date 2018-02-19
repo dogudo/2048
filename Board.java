ed0e64fe5e4047e9f6f68d7c57310f9b43953812
Alonso Bravo <alobrv@gmail.com>
2018-02-19 20:03:34 +0100

    Fixed initializing in constructor (thanks @ptato)

--- Board.java
+++ Board.java
@@ -4,8 +4,8 @@ public class Board {
 
   public Board() {
     board = new int[4][4];
-    for (int i = 0; i < 3; i++) {
-      for (int j = 0; j < 3; j++) {
+    for (int i = 0; i < 4; i++) {
+      for (int j = 0; j < 4; j++) {
         board[i][j] = 0;
       }
     }
