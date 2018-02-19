84992d62488eb1716a07bffe7bd64ff1c541344a
Alonso Bravo <alobrv@gmail.com>
2018-02-19 19:49:41 +0100

    First playable version

--- Board.java
+++ Board.java
@@ -20,7 +20,9 @@ public class Board {
     for (int i = 0; i < 4; i++) {
       for (int j = 0; j < 4; j++) {
         System.out.print("│");
-        if (board[i][j] < 10) {
+        if (board[i][j] == 0) {
+          System.out.print("    ");
+        } else if (board[i][j] < 10) {
           System.out.print("  " + board[i][j] + " ");
         } else if (board[i][j] < 100) {
           System.out.print(" " + board[i][j] + " ");
@@ -58,4 +60,76 @@ public class Board {
     }
   }
 
+  public void move(char c) {
+    int aux;
+    if (c == 'u') {
+      for (int i = 1; i < 4; i++) {
+        for (int j = 0; j < 4; j++) {
+          aux = i;
+          while (i > 0 && board[i-1][j] == 0) {
+            board[i-1][j] = board[i][j];
+            board[i][j] = 0;
+            i--;
+          }
+          if (i > 0 && board[i-1][j] == board[i][j]) {
+            board[i-1][j] = 2*board[i][j];
+            board[i][j] = 0;
+          }
+          i = aux;
+        }
+      }
+      spawn();
+    } else if (c == 'd') {
+      for (int i = 2; i > -1; i--) {
+        for (int j = 0; j < 4; j++) {
+          aux = i;
+          while (i < 3 && board[i+1][j] == 0) {
+            board[i+1][j] = board[i][j];
+            board[i][j] = 0;
+            i++;
+          }
+          if (i < 3 && board[i+1][j] == board[i][j]) {
+            board[i+1][j] = 2*board[i][j];
+            board[i][j] = 0;
+          }
+          i = aux;
+        }
+      }
+      spawn();
+    } else if (c == 'l') {
+      for (int j = 1; j < 4; j++){
+        for (int i = 0; i < 4; i++) {
+          aux = j;
+          while (j > 0 && board[i][j-1] == 0) {
+            board[i][j-1] = board[i][j];
+            board[i][j] = 0;
+            j--;
+          }
+          if (j > 0 && board[i][j-1] == board[i][j]) {
+            board[i][j-1] = 2*board[i][j];
+            board[i][j] = 0;
+          }
+          j = aux;
+        }
+      }
+      spawn();
+    } else if (c == 'r') {
+      for (int j = 2; j > -1; j--) {
+        for (int i = 0; i < 4; i++) {
+          aux = j;
+          while (j < 3 && board[i][j+1] == 0) {
+            board[i][j+1] = board[i][j];
+            board[i][j] = 0;
+            j++;
+          }
+          if (j < 3 && board[i][j+1] == board[i][j]) {
+            board[i][j+1] = 2*board[i][j];
+            board[i][j] = 0;
+          }
+          j = aux;
+        }
+      }
+      spawn();
+    }
+  }
 }
--- _2048.java
+++ _2048.java
@@ -1,9 +1,17 @@
+import java.util.Scanner;
+
 public class _2048 {
 
   public static void main (String args[]) {
+    Scanner reader = new Scanner(System.in);
+    char c;
     Board board = new Board();
     board.start();
-    board.draw();
+    while (true) {
+      board.draw();
+      c = reader.next().charAt(0);
+      board.move(c);
+    }
   }
 
 }
